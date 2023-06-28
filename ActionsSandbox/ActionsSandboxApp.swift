e650788b759efb3d8635f857844ffd0d6b987e66
tichinose1 <tichinose1dev@gmail.com>
2023-06-28 09:15:02 +0900

    Initial Commit

---
+++ ActionsSandbox/ActionsSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionsSandboxApp.swift
+//  ActionsSandbox
+//
+//  Created by tichinose1 on 2023/06/28.
+//
+
+import SwiftUI
+
+@main
+struct ActionsSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
